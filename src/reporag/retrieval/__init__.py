72a7f210693177655d7acd2b242c1d426776514e
zliel <zpliel@gmail.com>
2026-04-02 16:56:59 -0700

    Added dunder init files

--- src/reporag/retrieval/__init__.py
+++ src/reporag/retrieval/__init__.py
@@ -1,3 +1,18 @@
+from reporag.retrieval.context_files import (
+    ContextSection,
+    chunk_context_directory,
+    chunk_context_file,
+    chunk_context_path,
+    retrieve_context_sections,
+)
 from reporag.retrieval.search import RetrievedChunk, top_k_similar
 
-__all__ = ["RetrievedChunk", "top_k_similar"]
+__all__ = [
+    "RetrievedChunk",
+    "top_k_similar",
+    "ContextSection",
+    "chunk_context_file",
+    "chunk_context_directory",
+    "chunk_context_path",
+    "retrieve_context_sections",
+]
